e50eb5b890d0c8ac9e9088c6b87c4cb0a35b5c73
Mashuk25 <elahimashukh@gmail.com>
2025-12-13 12:03:56 +1300

    Scenario added

--- W3A4.py
+++ W3A4.py
@@ -1,7 +1,14 @@
 # The Scenario
-# Display the number of students enrolled in MSE800 (Professional Software Engineering).
+# Scenario
 
-# List all teachers teaching MSE801 (Research Methods).
+#Yoobee College offers postgraduate programmes that include several courses, students, and teachers. To manage academic information efficiently,
+#the college plans to develop a College Course Management System using Object-Oriented Programming (OOP), based on the ER diagram designed in
+#Week 3 – Activity 3. The system models core entities such as Student, Teacher, and Course, along with their relationships, to accurately represent
+#the college’s academic structure.
+
+#The system allows students to enroll in courses and teachers to be assigned to teach specific courses. It is designed to display the total number
+#of students enrolled in MSE800 (Professional Software Engineering) and to list all teachers teaching MSE801 (Research Methods). This project 
+#demonstrates how an ER diagram can be transformed into an OOP-based solution to handle real-world academic data management at Yoobee College.#
 
 # -------------------------------
 # OOP MODEL BASED ON YOUR COURSES
@@ -117,4 +124,5 @@ print("Number of students in MSE800:", count_students_MSE800())
 print("\nTeachers teaching MSE801:")
 for teacher in list_teachers_MSE801():
     print("-", teacher)
+
 
